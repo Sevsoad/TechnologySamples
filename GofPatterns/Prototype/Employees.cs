a36397ec5aff9aa4d008532b83c288648b18506d
Siarhei Bialko <sergey.bialko@gmail.com>
2016-11-14 11:08:50 +0300

    Prototype pattern add

---
+++ GofPatterns/Prototype/Employees.cs
@@ -0,0 +1,26 @@
+﻿namespace GofPatterns.Prototype
+{
+    public abstract class Employee
+    {
+        public string Name;
+        public abstract Employee Clone();
+    }
+
+    public class Qa : Employee
+    {
+        public int BugsPerHour;
+        public override Employee Clone()
+        {
+            return (Qa)MemberwiseClone();
+        }
+    }
+
+    public class Dev : Employee
+    {
+        public int CodePerHour;
+        public override Employee Clone()
+        {
+            return (Dev)MemberwiseClone();
+        }
+    }
+}
---
+++ GofPatterns/Prototype/PrototypeTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GofPatterns.Prototype
+{
+    [TestClass]
+    public class PrototypeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Dev dev = new Dev();
+            dev.Name = "Richard";
+            dev.CodePerHour = 140;
+
+            Qa qa = new Qa();
+            qa.Name = "Paul";
+            qa.BugsPerHour = 3;
+
+            Qa qaClone = (Qa)qa.Clone();
+            qaClone.Name = "Paul's clone";
+
+            System.Diagnostics.Trace.WriteLine(qa.Name);
+            System.Diagnostics.Trace.WriteLine(qaClone.Name);
+            System.Diagnostics.Trace.WriteLine(qaClone.BugsPerHour);
+        }
+    }
+}

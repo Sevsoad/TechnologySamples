c894db81ade08e88b909e9bad0bf40839ad86dd8
Siarhei Bialko <sergey.bialko@gmail.com>
2016-11-14 15:28:07 +0300

    Singleton add

---
+++ GofPatterns/Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace GofPatterns.Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance;
+        private static object _lock = new object();
+        public string InternalValue;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            lock (_lock)
+            {
+                _instance = _instance ?? new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
---
+++ GofPatterns/Singleton/SingletonTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GofPatterns.Singleton
+{
+    [TestClass]
+    public class SingletonTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var singletonObject = Singleton.GetInstance();
+
+            singletonObject.InternalValue = "Singleton#1";
+
+            var anotherSingletonObject = Singleton.GetInstance();
+
+            Assert.AreEqual(anotherSingletonObject.InternalValue, "Singleton#1");
+        }
+    }
+}

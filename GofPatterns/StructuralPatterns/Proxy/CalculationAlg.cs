e92f1351e7a53644794a896fda5ffde00b4f7bb0
sevsoad <sergey.bialko@gmail.com>
2016-11-16 00:53:48 +0300

    Cache proxy add

---
+++ GofPatterns/StructuralPatterns/Proxy/CalculationAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GofPatterns.StructuralPatterns.Proxy
+{
+    public abstract class CalculationAlgBase
+    {
+        public abstract int Calculate();
+    }
+
+    public class CalculationAlg : CalculationAlgBase
+    {
+        public override int Calculate()
+        {
+            //It's should be very slow operation, right?
+            System.Diagnostics.Trace.WriteLine("Making big calculation");
+            return 1000000;
+        }
+    }
+
+    public class CalculationAlgProxy : CalculationAlgBase
+    {
+        private CalculationAlg _calcAlgOriginal;
+        private int _cachedValue;
+
+        public override int Calculate()
+        {
+            if (_calcAlgOriginal == null)
+            {
+                _calcAlgOriginal = new CalculationAlg();
+                _cachedValue = _calcAlgOriginal.Calculate();
+            }
+
+            return _cachedValue;
+        }
+    }
+}
---
+++ GofPatterns/StructuralPatterns/Proxy/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GofPatterns.StructuralPatterns.Proxy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var calcAlg = new CalculationAlgProxy();
+
+            for (var i = 0; i < 5; i++)
+            {
+                var calculatedValue = calcAlg.Calculate();
+                System.Diagnostics.Trace.WriteLine(calculatedValue);
+            }
+        }
+    }
+}
